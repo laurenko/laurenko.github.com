7678e1d888e4a1212ee2ecfe6adea4b9d92dc8cf
laurenko <laurenmpe@gmail.com>
2019-11-04 14:02:00 +0700

    Update theme color,favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -83,7 +83,7 @@ class Layout extends React.Component {
           meta={[
             {
               name: 'theme-color',
-              content: this.state.theme === 'light' ? '#ffa8c5' : '#282c35',
+              content: this.state.theme === 'light' ? '#df78ef' : '#282c35',
             },
           ]}
         />
